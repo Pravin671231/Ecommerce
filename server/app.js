359330465f1f4a5e5d889c84fcaf2f32872e4ea3
Pravin671231 <pravinkumar671231@gmail.com>
2025-05-27 09:08:06 +0530

    Create user model and Register a

--- server/app.js
+++ server/app.js
@@ -6,7 +6,9 @@ const errorMiddlerware = require("./middlewares/error");
 app.use(express.json());
 
 const products = require("./routes/product");
+const auth = require("./routes/auth");
 app.use("/api/v1", products);
+app.use("/api/v1", auth);
 
 app.use(errorMiddlerware);
 
