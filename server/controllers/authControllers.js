8b24053fef19de7848e1a23ec13b9df4b5b28166
Pravin671231 <pravinkumar671231@gmail.com>
2025-05-28 00:29:17 +0530

    Generate reset Password token and Forget password API send email

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,6 @@
 const catchAsyncError = require("../middlewares/catchAsyncError");
 const User = require("../models/userModel");
+const sendEmail = require("../utils/email");
 const errorHandler = require("../utils/errorHandler");
 const sendToken = require("../utils/jwt");
 
@@ -44,3 +45,39 @@ exports.logoutUser = (req, res, next) => {
       message: "Logout user",
     });
 };
+
+exports.forgetPassword = catchAsyncError(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new errorHandler("User not found with email", 404));
+  }
+
+  const resetToken = user.getResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  //create reset Url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/auth/password/reset/${resetToken}`;
+
+  const message = `Your password reset url is as follows \n\n ${resetUrl}\n\n If not requested this email,then ignore it. `;
+
+  try {
+    sendEmail({
+      email: user.email,
+      subject: "Ecommerce password recovery",
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email}`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTokenExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new errorHandler(error.message, 500));
+  }
+});
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, "Please enter name"] },
@@ -31,6 +32,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function (next) {
+   if (!this.isModified("password")) {
+    return next();
+  }
   this.password = await bcrypt.hash(this.password, 10);
 });
 userSchema.methods.getJwtToken = function () {
@@ -40,6 +44,19 @@ userSchema.methods.getJwtToken = function () {
 };
 userSchema.methods.isValidPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.methods.getResetToken = function () {
+  //Generate token
+  const token = crypto.randomBytes(20).toString("hex");
+  //Generate Hash and set to resetPasswordToken
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+  //Set token expires time
+  this.resetPasswordTokenExpire = Date.now() + 30 * 60 * 1000;
+
+  return token;
 };
 
 let model = mongoose.model("user", userSchema);
